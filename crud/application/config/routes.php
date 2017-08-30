043b883477ebfdfef0ae2124484da406424c59e2
jakelinebissani <jakeline.bissani@gmail.com>
2017-08-30 04:55:09 -0300

    refazendo do 0

--- crud/application/config/routes.php
+++ crud/application/config/routes.php
@@ -49,11 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'Base';
+$route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-
-$route['salvar'] = "Base/Salvar";
-$route['editar/(:num)'] = "Base/Editar/$1";
-$route['atualizar'] = "Base/Atualizar";
-$route['excluir/(:num)'] = "Base/Excluir/$1";
